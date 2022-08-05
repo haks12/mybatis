659ada3efca3c283f0388bc50a8d3b2eb74d216c
unknown <a@a.com>
2022-08-05 17:44:12 +0900

    '0805'

---
+++ sql/mybatis2.sql
@@ -0,0 +1,21 @@
+select * from users;
+
+delete users
+where user_id = 11;
+
+desc users
+
+commit;
+
+delete users
+where user_id = 12;
+
+delete users
+where user_id = 13;
+
+delete users
+where user_id = 14;
+
+select * from users;
+
+commit;
